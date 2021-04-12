3f86f75ef3ad6f306755118684ca70298123ea7f
unknown <muneebali500@gmail.com>
2021-04-12 21:59:51 +0500

    app-completed

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.scss";
+
+function Footer() {
+  return (
+    <footer>
+      <p className={styles.Copyright}>All the © are reserved by Traville' 🙂</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,56 @@
+const PriceData = [
+  {
+    id: 1,
+    amount: `💰299`,
+    policy: `Standard`,
+    details: [
+      `🚴 2 days 3 nights`,
+      `🚴 Upto 25 people`,
+      `🚴 2 tour guides`,
+      `🚴 Sleep in cozy hotels`,
+      `🚴 Swimming pool facility`,
+    ],
+  },
+  {
+    id: 2,
+    amount: `💰499`,
+    policy: `Business`,
+    details: [
+      `🤸 4 days 5 nights`,
+      `🤸 Upto 35 people`,
+      `🤸 4 tour guides`,
+      `🤸 Sleep in cozy hotels`,
+      `🤸 Swimming pool facility`,
+    ],
+  },
+  {
+    id: 3,
+    amount: `💰899`,
+    policy: `Premium`,
+    details: [
+      `🤼 6 days 7 nights`,
+      `🤼 Upto 50 people`,
+      `🤼 6 tour guides`,
+      `🤼 Sleep in cozy hotels`,
+      `🤼 Swimming pool facility`,
+    ],
+  },
+];
+
+const gallery = [
+  `Derawar.jpg`,
+  `group-univ.jpg`,
+  `img-1.jpg`,
+  `Katas_Raj.jpg`,
+  `LahoreFort.jpg`,
+  `mahodand.jpg`,
+  `Mushkpuri.jpg`,
+  `Pakistan_Monument.jpg`,
+  `people.jpg`,
+  `princesOfHope.jpg`,
+  `Shah-Rukne-Alam-Mazar.jpg`,
+  `Shalimar.jpg`,
+];
+
+export default PriceData;
+export { gallery };
