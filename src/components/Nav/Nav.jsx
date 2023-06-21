9f30147a80a511d06c6333e723e3f7cc1ef84e85
Muneeb Ali <muneebali500@gmail.com>
2023-06-21 20:51:28 +0500

    fixed nav alignmnet

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import styles from "./nav.module.scss";
 
 function Nav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <div className={styles.Logo}>
         <h3>Traville</h3>
       </div>
